6331b14ebb78fbd3b6b4bbc060f4c446060070bd
Yadiel <yadiel1024@outlook.es>
2026-03-29 21:42:58 -0300

    5125

--- js/Login.js
+++ js/Login.js
@@ -1,37 +1,46 @@
-// Login
-document.getElementById("loginForm").addEventListener("submit", function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
+// Cambiar entre login y registro
+function mostrarRegistro(){
+  document.getElementById("loginBox").classList.add("hidden");
+  document.getElementById("registerBox").classList.remove("hidden");
+}
 
-    fetch("php/login.php", {method:"POST", body: formData})
-    .then(r=>r.json())
-    .then(data=>{
-        if(data.success){
-            alert("Login exitoso!");
-            window.location.href = "panel_cliente.php"; // Página privada del cliente
-        }else{
-            alert(data.message);
-        }
-    });
-});
+function mostrarLogin(){
+  document.getElementById("registerBox").classList.add("hidden");
+  document.getElementById("loginBox").classList.remove("hidden");
+}
 
-// Registro
+// MODAL
+function mostrarModal(tipo){
+  const modal = document.getElementById("modal");
+  const contenido = document.getElementById("contenidoModal");
+
+  if(tipo === "privacidad"){
+    contenido.innerHTML = `
+    <h3>Política de Privacidad</h3>
+    <p>En PixelPro protegemos tus datos personales. No compartimos tu información con terceros y solo se utiliza para gestionar tus pedidos.</p>
+    `;
+  }
+
+  if(tipo === "terminos"){
+    contenido.innerHTML = `
+    <h3>Términos y Condiciones</h3>
+    <p>Al usar PixelPro aceptas que los pedidos realizados deben ser pagados antes de la entrega. La empresa no se responsabiliza por errores enviados por el cliente.</p>
+    `;
+  }
+
+  modal.style.display = "flex";
+}
+
+function cerrarModal(){
+  document.getElementById("modal").style.display = "none";
+}
+
+// VALIDACIÓN BÁSICA
 document.getElementById("registerForm").addEventListener("submit", function(e){
+  if(!document.getElementById("terms").checked){
+    alert("Debes aceptar los términos");
     e.preventDefault();
-    if(!document.getElementById("terms").checked){
-        alert("Debes aceptar los términos");
-        return;
-    }
-    let formData = new FormData(this);
-
-    fetch("php/registro.php", {method:"POST", body: formData})
-    .then(r=>r.json())
-    .then(data=>{
-        if(data.success){
-            alert("Cuenta creada correctamente!");
-            mostrarLogin();
-        }else{
-            alert(data.message);
-        }
-    });
+  }else{
+    alert("Cuenta creada ");
+  }
 });

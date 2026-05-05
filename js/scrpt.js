7c8b92b89f77a443f2d4cec4599f4098cbed83c5
Antony <antonymontero28@outlook.es>
2026-05-04 22:30:22 -0400

    m

--- js/scrpt.js
+++ js/scrpt.js
@@ -4,3 +4,11 @@ console.log("Proyecto cargado correctamente");
 const track = document.querySelector(".reviews-track");
 
 track.innerHTML += track.innerHTML;
+
+const menu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    menu.classList.toggle("active"); // opcional para animar las barras
+});

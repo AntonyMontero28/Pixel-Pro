afd5153838cadc44531f897760ac337db1d2e514
Antony <antonymontero28@outlook.es>
2026-05-05 01:08:47 -0400

    mas arreglos

--- js/noticias.js
+++ js/noticias.js
@@ -1,12 +1,32 @@
- let cards = document.querySelectorAll(".card");
+let cards = document.querySelectorAll(".card");
 
-cards.forEach(card => {
-    card.addEventListener("click", function(){
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    // Quita el efecto a todos
+    cards.forEach((c) => c.classList.remove("activo"));
 
-        // Quita el efecto a todos
-        cards.forEach(c => c.classList.remove("activo"));
+    // Activa solo el que tocaste
+    card.classList.add("activo");
+  });
+});
 
-        // Activa solo el que tocaste
-        card.classList.add("activo");
-    });
+const menu = document.getElementById("mobile-menu");
+const nav = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+menu.addEventListener("click", () => {
+  // activar menú
+  nav.classList.toggle("nav-active");
+
+  // animación de links
+  links.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    }
+  });
+
+  // animación hamburguesa
+  menu.classList.toggle("toggle");
 });
